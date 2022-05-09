62612c0693c38d8b3d8ca48693c0504cbbdb609f
clarxyg <clarxyg@gmail.com>
2022-05-09 09:56:20 -0300

    first commit

--- src/use-cases/submit-feedback-use-case.ts
+++ src/use-cases/submit-feedback-use-case.ts
@@ -41,6 +41,7 @@ export class SubmitFeedbackUseCase {
                 `<div style="font-family: sans-serif; font-size: 1rem; color: #111;">`,
                 `<p>Tipo do feedback: ${type}</p>`, 
                 `<p>Comentário: ${comment}</p>`,
+                 screenshot ? `<img src="${screenshot}"` : null,
                 `</div>`
             ].join('\n')
        })
